866c161665e8228b32e7b097a68d2f39edb51607
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-08-27 13:30:08 +0530

    login fuction

--- src/services/user.ts
+++ src/services/user.ts
@@ -8,11 +8,24 @@ export interface CreateUserPayload {
   password: string
 }
 
+export interface GetUserTokenPayload {
+  email: string;
+  password: string;
+}
+
 class UserService {
+  
+  private static generateHash(salt: string, password: string) {
+    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex');
+    return hashedPassword;
+  }
+
   public static createUser(payload:CreateUserPayload) {
     const {firstName, lastName, email, password} = payload;
-    const salt = randomBytes(32).toString();
-    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex'); 
+    
+    const salt = randomBytes(32).toString('hex');
+    const hashedPassword = UserService.generateHash(salt, password);
+
     return prismaClient.user.create({
       data: {
         firstName,
@@ -22,6 +35,24 @@ class UserService {
         password: hashedPassword,
       }
     });
+  }
+
+  private static getUserByEmail(email: string) {
+    return prismaClient.user.findUnique({where: {email}});
+  }
+  
+  public static async getUserToken(payload: GetUserTokenPayload) {
+    const {email, password} = payload;
+    const user = await UserService.getUserByEmail(email);
+    if(!user) throw new Error('user not found');
+
+    const userSalt = user.salt;
+    const usersHashPassword = UserService.generateHash(userSalt, password);
+
+    if(usersHashPassword != user.password) throw new Error('Incorrect Password')
+
+    //Gen a token
+    
   }
 }
 
